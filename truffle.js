4d77aae6c77468774d1e80f9e7355e0f7016abbf
pertsat0 <tpertsas@gmail.com>
2017-10-29 03:44:19 +0000

    improvement of packaging and network

--- truffle.js
+++ truffle.js
@@ -1,9 +1,13 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "1337", // Match any network id
+      gas: 4712388
     }
   }
-};
+}
